cae68dd29c3a9f710495b1d979cbc4fea9598bb2
Halim Arumman <halimarumman@gmail.com>
2021-02-20 21:49:47 +0700

    BREAKING CHANGE: upgrade feature v1 to v2
    BREAKING CHANGE: upgrade feature v1 to v2
    - Feature 1  - v2
    - Feature 2 - v2
    - Feature 3 - v2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 console.log('+-- Semantic Release --+');
 
-console.log('Feature 1');
-console.log('Feature 2');
-console.log('Feature 3');
+console.log('Feature 1 - v2');
+console.log('Feature 2 - v2');
+console.log('Feature 3 - v2');
